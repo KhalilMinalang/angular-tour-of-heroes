1f028c038f317062731ab9efddda9d1a8677ba40
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-31 07:20:28 +0800

    COMPLETED Ch5: Add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
